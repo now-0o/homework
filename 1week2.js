b751be44068188311d237d1fcec83c38dbc9c4fd
now_0o <dlwpdn0704@gmail.com>
2023-09-09 19:50:09 +0900

    과제 3-2 세번째

--- 1week2.js
+++ 1week2.js
@@ -25,3 +25,13 @@ function check_score(score){
 }
 
 console.log(check_score(90));
+
+// 세 개의 숫자를 입력받아 가장 큰 숫자를 찾아서 반환(return)하는 함수를 작성해라. 
+
+function find_max(num1, num2, num3){
+    const arry = [num1,num2,num3];
+
+    return Math.max(...arry);
+}
+
+console.log(find_max(1,5,8));
